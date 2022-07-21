1e9e7cdaedf03d14eeffc09b8ef2c5055057a91b
Dominik Bułaj <dominik@bulaj.com>
2022-07-21 10:58:10 +0200

    changed API protocol to https for geo checking

--- src/hooks/useSelectCity.ts
+++ src/hooks/useSelectCity.ts
@@ -24,7 +24,7 @@ const useSelectCity = () => {
 
 
                     const apiKey = process.env.REACT_APP_OPENWEATHERMAP_API_KEY
-                    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
+                    const response = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
                     const json = await response.json() as CitesList | ApiErrorResponse
 
                     if (isApiError<CitesList>(json)) {
